55d6357f93013cd68dada7b240b12e5839f12a1e
ljihyun99 <Ji.Lee@campus.lmu.de>
2024-12-16 23:01:01 +0100

    youtube api succesfully extracted data

--- etls/youtube_etl.py
+++ etls/youtube_etl.py
@@ -3,9 +3,11 @@ import json
 import pandas as pd
 
 class YoutubeETL:
-    def __init__(self, api_key, keyword):
+    def __init__(self, api_key, keyword, next_page_token, page_number):
         self.api_key = api_key
         self.keyword = keyword
+        self.next_page_token = next_page_token
+        self.page_number = page_number
 
     def connect_youtube(self):
         """Connect to the YouTube API and return an instance."""
@@ -14,16 +16,31 @@ class YoutubeETL:
 
     def extract(self, youtube_instance, max_results, published_after, published_before):
         """Extract data from the YouTube API."""
-        
-        request = youtube_instance.search().list(
-            q = self.keyword,
-            order="relevance",
-            part="snippet",
-            maxResults=max_results,
-            publishedAfter=published_after,
-            publishedBefore=published_before
-        )
+        if self.page_number == 0:
+            request = youtube_instance.search().list(
+                q = self.keyword,
+                order="relevance",
+                part="snippet",
+                maxResults=max_results,
+                publishedAfter=published_after,
+                publishedBefore=published_before
+            )
+
+        else:
+            request = youtube_instance.search().list(
+                q = self.keyword,
+                order="relevance",
+                part="snippet",
+                maxResults=max_results,
+                publishedAfter=published_after,
+                publishedBefore=published_before,
+                pageToken = self.next_page_token
+            )
+
         response = request.execute()
+        self.next_page_token = response["nextPageToken"]
+        self.page_number += 1
+        
         return response
     
     def convert_to_json(self, response):
@@ -32,7 +49,7 @@ class YoutubeETL:
         else:
             raw_data = json.dumps(response)
 
-            with open(f"data/raw_data/{self.keyword}.json", "w") as file:   
+            with open(f"data/raw_data/{self.keyword}_{self.page_number}.json", "w") as file:   
                 file.write(raw_data)
 
         return raw_data
--- pipelines/youtube_pipeline.py
+++ pipelines/youtube_pipeline.py
@@ -11,9 +11,10 @@ from etls.youtube_etl import YoutubeETL
 maxResults = 50
 publishedAfter = '2024-12-01T00:00:00Z'
 publishedBefore = '2024-12-15T00:00:00Z'
-etl_pipeline = YoutubeETL(api_key='AIzaSyBOOtd-JArcnY_jxwcRP_l150LDcp_Igq4', keyword = 'Korea')
+etl_pipeline = YoutubeETL(api_key=<your_google_api_key>, keyword = 'Korea', next_page_token=None, page_number=0)
 
 raw_json_data = None
+response = None
 
 def youtube_pipeline():
     """Run the YouTube ETL pipeline."""
@@ -21,14 +22,21 @@ def youtube_pipeline():
     instance = etl_pipeline.connect_youtube()
     
     print("extracting through youtube data api")
+
     # Extract data
-    response = etl_pipeline.extract(instance,  maxResults, publishedAfter, publishedBefore)
+    while etl_pipeline.page_number <= 10:
+        response = etl_pipeline.extract(instance,  maxResults, publishedAfter, publishedBefore)
+        
+        if response is not None:
+            print("extracted successfully")
 
-    print("extracted successfully")
+        raw_data = etl_pipeline.convert_to_json(response)
 
-    raw_data = etl_pipeline.convert_to_json(response)
+        if raw_data is not None:
+            print(f"converted into JSON and saved it into raw_data folder: {etl_pipeline.keyword} for page {etl_pipeline.page_number}")     
 
-    print("converted into JSON and saved it into raw_data folder")
+        response = None
+        raw_data = None
 
     # Transformation step (add logic later)
     # Example: df_transformed = transform(response)
@@ -36,13 +44,8 @@ def youtube_pipeline():
     # Load step (add logic later)
     # Example: load_data_to_csv(df_transformed, 'youtube_data.csv')
 
-    return raw_data
-
 # Run the pipeline
-raw_json_data = youtube_pipeline()
-
-# Print the response (for testing)
-print(raw_json_data)
+youtube_pipeline()
 
 
 
